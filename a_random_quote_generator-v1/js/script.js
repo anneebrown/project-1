7185508e3d4e93239c115e9a894770f1964be099
anneebrown <61443022+anneebrown@users.noreply.github.com>
2020-02-25 19:33:00 -0800

    added preliminary code
    added quotes array, getrandomNumber function, first version of printQuotes function

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,20 +10,55 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+let quotes = [
+  {quote: "You may encounter many defeats, but you must not be defeated. In fact, it may be necessary to encounter the defeats, so you can know who you are, what you can rise from, how you can still come out of it.", 
+   source: "Maya Angelou", 
+   citation: "Interview with psychology today", 
+   year: 2009
+  },
+  {quote: "Even if we don't have the power to choose where we come from, we can still choose where we go from there." ,
+   source: "Stephen Chbosky", 
+   citation: "Perks of being a wallflower",
+   year: 1999
+  },
+  {quote: "To finish the moment, to find the journey’s end in every step of the road, to live the greatest number of good hours, is wisdom.", 
+   source: "Ralph Waldo Emerson",
+   citation: "Essays and Lectures",
+   year: 1848
+  },
+  {quote: "Great works are performed not by strength but by perseverance.",
+   source: "Samuel Johnson",
+   citation: "Rasselas",
+   year: 1759
+  },
+  {quote: "To thine own self be true.",
+   source: "William Shakespeare",
+   citation: "Hamlet", 
+   year: 1603
+  },
+];
 
 
 /***
  * `getRandomQuote` function
+ * Runs a random number generator first to select an item in the array
 ***/
-
+function getRandomQuote() {
+ let randomNumber = Math.floor(Math.random() * quotes.length);
+ return randomNumber;
+}
 
 
 /***
  * `printQuote` function
 ***/
-
-
+function printQuote() { 
+  print(`<h1>${quotes[randomNumber]}</h1> 
+  -quotes[randomNumber][1]`)
+  if (quotes[randomNumber][1] !== undefined){
+    print(`$quotes[randomNumber][1]`)
+  }
+}
 
 /***
  * click event listener for the print quote button
