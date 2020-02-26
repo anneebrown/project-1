861895cc1eac706d3da194dcfaaaab12bd77e4b1
anneebrown <61443022+anneebrown@users.noreply.github.com>
2020-02-25 21:50:39 -0800

    some test code
    not finished

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -11,38 +11,20 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 let quotes = [
-  {quote: "You may encounter many defeats, but you must not be defeated. In fact, it may be necessary to encounter the defeats, so you can know who you are, what you can rise from, how you can still come out of it.", 
-   source: "Maya Angelou", 
-   citation: "Interview with psychology today", 
-   year: 2009
-  },
-  {quote: "Even if we don't have the power to choose where we come from, we can still choose where we go from there." ,
-   source: "Stephen Chbosky", 
-   citation: "Perks of being a wallflower",
-   year: 1999
-  },
-  {quote: "To finish the moment, to find the journey’s end in every step of the road, to live the greatest number of good hours, is wisdom.", 
-   source: "Ralph Waldo Emerson",
-   citation: "Essays and Lectures",
-   year: 1848
-  },
-  {quote: "Great works are performed not by strength but by perseverance.",
-   source: "Samuel Johnson",
-   citation: "Rasselas",
-   year: 1759
-  },
-  {quote: "To thine own self be true.",
-   source: "William Shakespeare",
-   citation: "Hamlet", 
-   year: 1603
-  },
+  ['You may encounter many defeats, but you must not be defeated. In fact, it may be necessary to encounter the defeats, so you can know who you are, what you can rise from, how you can still come out of it.', 'Maya Angelou', 'Interview with psychology today', 2009]
+  ['Even if we don\'t have the power to choose where we come from, we can still choose where we go from there.', 'Stephen Chbosky', 'Perks of being a wallflower', 1999],
+  ['To finish the moment, to find the journey\'s end in every step of the road, to live the greatest number of good hours, is wisdom.', 'Ralph Waldo Emerson',  'Essays and Lectures', 1848],
+  ['Great works are performed not by strength but by perseverance.', 'Samuel Johnson', 'Rasselas', 1759],
+  ['To thine own self be true.', 'William Shakespeare', 'Hamlet', 1603],
 ];
 
 
 /***
  * `getRandomQuote` function
  * Runs a random number generator first to select an item in the array
+ * The random number generator is based on the random number generators taught in various treehouse lessons.
 ***/
+let randomNumber;
 function getRandomQuote() {
  let randomNumber = Math.floor(Math.random() * quotes.length);
  return randomNumber;
@@ -51,14 +33,12 @@ function getRandomQuote() {
 
 /***
  * `printQuote` function
+ * 
 ***/
 function printQuote() { 
-  print(`<h1>${quotes[randomNumber]}</h1> 
-  -quotes[randomNumber][1]`)
-  if (quotes[randomNumber][1] !== undefined){
-    print(`$quotes[randomNumber][1]`)
-  }
-}
+  var quoteOnPage = document.getElementById('quote-box');
+  quoteOnPage.innerHTML = `${quotes[randomNumber]}`;
+};
 
 /***
  * click event listener for the print quote button
