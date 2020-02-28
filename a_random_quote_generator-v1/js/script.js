13b1b413d78ff613da1145ed3b47163948cdd905
anneebrown <61443022+anneebrown@users.noreply.github.com>
2020-02-27 22:34:28 -0800

    almost finished
    almost finished, need to work on getrandomquote function

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -11,11 +11,31 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 let quotes = [
-  ['You may encounter many defeats, but you must not be defeated. In fact, it may be necessary to encounter the defeats, so you can know who you are, what you can rise from, how you can still come out of it.', 'Maya Angelou', 'Interview with psychology today', 2009]
-  ['Even if we don\'t have the power to choose where we come from, we can still choose where we go from there.', 'Stephen Chbosky', 'Perks of being a wallflower', 1999],
-  ['To finish the moment, to find the journey\'s end in every step of the road, to live the greatest number of good hours, is wisdom.', 'Ralph Waldo Emerson',  'Essays and Lectures', 1848],
-  ['Great works are performed not by strength but by perseverance.', 'Samuel Johnson', 'Rasselas', 1759],
-  ['To thine own self be true.', 'William Shakespeare', 'Hamlet', 1603],
+  {quote: "You may encounter many defeats, but you must not be defeated. In fact, it may be necessary to encounter the defeats, so you can know who you are, what you can rise from, how you can still come out of it.", 
+   source: "Maya Angelou", 
+   citation: "Interview with psychology today", 
+   year: 2009
+  },
+  {quote: "Even if we don't have the power to choose where we come from, we can still choose where we go from there." ,
+   source: "Stephen Chbosky", 
+   citation: "Perks of being a wallflower",
+   year: 1999
+  },
+  {quote: "To finish the moment, to find the journey’s end in every step of the road, to live the greatest number of good hours, is wisdom.", 
+   source: "Ralph Waldo Emerson",
+   citation: "Essays and Lectures",
+   year: 1848
+  },
+  {quote: "Great works are performed not by strength but by perseverance.",
+   source: "Samuel Johnson",
+   citation: "Rasselas",
+   year: 1759
+  },
+  {quote: "To thine own self be true.",
+   source: "William Shakespeare",
+   citation: "Hamlet", 
+   year: 1603
+  },
 ];
 
 
@@ -26,19 +46,41 @@ let quotes = [
 ***/
 let randomNumber;
 function getRandomQuote() {
- let randomNumber = Math.floor(Math.random() * quotes.length);
- return randomNumber;
+randomNumber = Math.floor(Math.random() * quotes.length);
+return randomNumber;
 }
+
 
 
 /***
  * `printQuote` function
  * 
 ***/
-function printQuote() { 
-  var quoteOnPage = document.getElementById('quote-box');
-  quoteOnPage.innerHTML = `${quotes[randomNumber]}`;
-};
+function printQuote() {
+  
+  // calling the getRandomQuote function
+  let numberToUse = getRandomQuote();
+  //starting the HTML string for the quote and source used, those will always appear, storing it in a new message variable
+ let message = `<p class="quote">${quotes[numberToUse].quote}</p>
+ <p class="source">${quotes[numberToUse].source}`;
+
+  //test to see if a citation is present, if so, add the citation to the message
+  if (quotes[numberToUse].citation !== undefined){
+    message += `<span class="citation"> ${quotes[numberToUse].citation}</span>`;
+  }
+ //test to see if a year is present, if so, add the year to the message
+if (quotes[numberToUse].year !== undefined){
+  message += `<span class=year>${quotes[numberToUse].year}</span>`;
+}
+ //close the HTML string
+  message += `</p>`
+
+  //set the innerHTML for the string, followed the project walkthrough guide for my printeQuote function. 
+  let printToPage = document.getElementById('quote-box');
+  printToPage.innerHTML = message; 
+
+}
+
 
 /***
  * click event listener for the print quote button
